3b3e6f0b5d7a253a42e08254503d01e7ede1beb6
nhanh <awmtrg21@gmail.com>
2020-03-14 12:50:53 +0700

    init repo

---
+++ OneEdu.HaGiangHomepage/wwwroot/js/site.js
@@ -0,0 +1,51 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+    infinite: false,
+    arrows: false,
+    asNavFor: '.slider-for',
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    focusOnSelect: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                arrows: true,
+                centerMode: true,
+            }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                arrows: true,
+                centerMode: true,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                arrows: true,
+                centerMode: true,
+            }
+        }
+    ]
+});
